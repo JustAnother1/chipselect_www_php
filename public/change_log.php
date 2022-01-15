a0e64b837f211edd30225f1522b77e1dc6389ce4
Lars Pötter <kontakt@ing-poetter.de>
2022-01-15 06:42:37 +0100

    make sure that only admins can accept changes.

--- public/change_log.php
+++ public/change_log.php
@@ -17,12 +17,16 @@ if((isset($_SESSION['login'])) && ($_SESSION["login"] == 1))
     include ("../secret.inc");
     $pdo = new PDO('mysql:dbname=microcontrollis;host=' . $db_host, $db_user, $db_password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
-    if(isset($_GET['id']))
+    if(in_array("admin", $_SESSION['user_roles']))
     {
-        // user clicked on Accept already -> store that in the db.
-        $stmt = $pdo->prepare( "UPDATE p_log SET accepted_by = ? WHERE id = ?");
-        $stmt->execute(array( $_SESSION['name'], $_GET['id']));
+        if(isset($_GET['id']))
+        {
+            // user clicked on Accept already -> store that in the db.
+            $stmt = $pdo->prepare( "UPDATE p_log SET accepted_by = ? WHERE id = ?");
+            $stmt->execute(array( $_SESSION['name'], $_GET['id']));
+        }
     }
+
     $sql = "SELECT id, action, on_table, on_id, on_column, old_value, new_value, user, time_of_change from p_log WHERE accepted_by IS NULL";
 
     echo("<table>\n");
