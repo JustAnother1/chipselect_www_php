90249061421d8f031362281d6596526893b3c15a
Lars Pötter <kontakt@ing-poetter.de>
2022-06-06 04:06:44 +0200

    added Flash Banks and RAm address and size support.

---
+++ public/rest/flash_bank.php
@@ -0,0 +1,73 @@
+<?php
+
+include("sanitize.inc");
+include("limit.inc");
+include("authenticate.inc");
+
+include("action_get.inc");
+include("action_post.inc");
+include("action_put.inc");
+include("action_delete.inc");
+
+$req_type = $_SERVER['REQUEST_METHOD'];
+header('Content-Type: application/json');
+include ("../../secret.inc");
+$pdo = new PDO('mysql:dbname=microcontrollis;host=' . $db_host, $db_user, $db_password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+switch ($req_type) {
+    case 'GET':
+        if(isset($_GET["dev_id"]))
+        {
+            $_GET["pl_flash_bank.dev_id"] = $_GET["dev_id"];
+        }
+        $opts = array(
+        "sql" => "SELECT id, start_address, size"
+            . " FROM p_flash_bank"
+            . " INNER JOIN pl_flash_bank ON pl_flash_bank.flash_id = p_flash_bank.id",
+        "filters" => array( "pl_flash_bank.dev_id"),
+        "allowUnfiltered" => false,
+        "hexColumns" => array( "start_address", "size" ),
+        );
+        handle_get($pdo, $opts);
+        break;
+
+    case 'POST':
+        $opts = array(
+        "entryId" => "start_address",
+        "table" => "p_flash_bank",
+        "columns" => array( "size" ),
+        "link_id" => "dev_id",
+        "lookup_table" => "pl_flash_bank",
+        "lookup_reference" => "dev_id",
+        "lookup_added_id" => "flash_id",
+        );
+        handle_post($pdo, $opts);
+        break;
+
+    case 'PUT':
+        $opts = array(
+        "filters" => array( "id" ),
+        "sql" => "SELECT id, start_address, size FROM p_flash_bank",
+        "table" => "p_flash_bank",
+        "columns" => array( "start_address", "size"),
+        );
+        handle_put($pdo, $opts);
+        break;
+
+    case 'DELETE':
+        $opts = array(
+        "filters" => array( "id" ),
+        "sql" => "SELECT id, start_address, size FROM p_flash_bank",
+        "table" => "p_flash_bank",
+        "columns" => array( "start_address", "size" ),
+        );
+        handle_delete($pdo, $opts);
+        break;
+        break;
+
+    default:
+        header('HTTP/1.0 501 Not Implemented');
+        break;
+} // end of switch
+
+$pdo = null;
+?>
--- public/rest/microcontroller.php
+++ public/rest/microcontroller.php
@@ -23,6 +23,7 @@ switch ($req_type) {
                . ", svd_id, Addressable_unit_bit, bus_width_bit"
                . ", description"
                . ", architecture_id, market_state_id, package_id, vendor_id"
+               . ", RAM_size_byte, RAM_start_address"
                . " FROM microcontroller",
         "filters" => array( "id", "name"),
         "allowUnfiltered" => true,
@@ -36,7 +37,7 @@ switch ($req_type) {
         "table" => "microcontroller",
         "columns" => array( "CPU_clock_max_MHz", "Flash_size_kB", "RAM_size_kB", "Supply_Voltage_min_V", "Supply_Voltage_max_V",
         "Operating_Temperature_min_degC", "Operating_Temperature_max_degC", "svd_id", "Addressable_unit_bit", "bus_width_bit",
-        "description", "architecture_id", "market_state_id", "package_id", "vendor_id" ),
+        "description", "architecture_id", "market_state_id", "package_id", "vendor_id", "RAM_size_byte", "RAM_start_address"),
         );
         handle_post($pdo, $opts);
         break;
@@ -51,11 +52,13 @@ switch ($req_type) {
                . ", svd_id, Addressable_unit_bit, bus_width_bit"
                . ", description"
                . ", architecture_id, market_state_id, package_id, vendor_id"
+               . ", RAM_size_byte, RAM_start_address"
                . " FROM microcontroller",
         "table" => "microcontroller",
         "columns" => array( "name", "CPU_clock_max_MHz", "Flash_size_kB", "RAM_size_kB", "Supply_Voltage_min_V",
         "Supply_Voltage_max_V", "Operating_Temperature_min_degC", "Operating_Temperature_max_degC", "svd_id",
-        "Addressable_unit_bit", "bus_width_bit", "description", "architecture_id", "market_state_id", "package_id", "vendor_id" ),
+        "Addressable_unit_bit", "bus_width_bit", "description", "architecture_id", "market_state_id", "package_id", "vendor_id",
+        "RAM_size_byte", "RAM_start_address"),
         );
         handle_put($pdo, $opts);
         break;
@@ -70,11 +73,13 @@ switch ($req_type) {
                . ", svd_id, Addressable_unit_bit, bus_width_bit"
                . ", description"
                . ", architecture_id, market_state_id, package_id, vendor_id"
+               . ", RAM_size_byte, RAM_start_address"
                . " FROM microcontroller",
         "table" => "microcontroller",
         "columns" => array( "name", "CPU_clock_max_MHz", "Flash_size_kB", "RAM_size_kB", "Supply_Voltage_min_V",
         "Supply_Voltage_max_V", "Operating_Temperature_min_degC", "Operating_Temperature_max_degC", "svd_id",
-        "Addressable_unit_bit", "bus_width_bit", "description", "architecture_id", "market_state_id", "package_id", "vendor_id" ),
+        "Addressable_unit_bit", "bus_width_bit", "description", "architecture_id", "market_state_id", "package_id", "vendor_id",
+        "RAM_size_byte", "RAM_start_address"),
         );
         handle_delete($pdo, $opts);
         break;
